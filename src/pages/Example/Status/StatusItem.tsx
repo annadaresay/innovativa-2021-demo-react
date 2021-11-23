c115fdf6678d65c9e9b3fba8b68e8fbaa623f6d9
Anna Viklund <anna.viklund@daresay.co>
2021-11-23 16:19:15 +0100

    Calculate scaled based on new server response

--- src/pages/Example/Status/StatusItem.tsx
+++ src/pages/Example/Status/StatusItem.tsx
@@ -10,17 +10,16 @@ const MAX_SCALE = 1.0;
 const useEmotionScale = (emotionId: string) => {
   const [scale, setScale] = useState(MIN_SCALE);
   const teamEventMean = useTeamEventMean();
-  const emotionMean = _.round(
+  const eventValue = _.round(
     teamEventMean?.find((m) => m.type === emotionId)?.value ?? 0,
     2
   );
 
-  console.log(`event mean (${emotionId}): `, emotionMean);
-
   // Update scale when emotion mean value is changed
   useEffect(() => {
-    setScale(_.clamp(emotionMean, MIN_SCALE, MAX_SCALE));
-  }, [emotionMean]);
+    const newScale = eventValue / (1 + eventValue); // [0, inf] -> [0, 1]
+    setScale(_.clamp(newScale, MIN_SCALE, MAX_SCALE));
+  }, [eventValue]);
 
   return scale;
 };
